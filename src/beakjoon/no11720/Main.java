ded8ca1fc2feefe7287d71748470f245bfb37872
jeahyunyoo <choozx23@gmail.com>
2021-09-23 19:21:09 +0900

    addEachNumToArray

---
+++ src/beakjoon/no11720/Main.java
@@ -0,0 +1,23 @@
+package beakjoon.no11720;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int[] numList = new int[sc.nextInt()];
+        String num = sc.next();
+        int total = 0;
+
+        for (int i=0;i< numList.length;i++){
+            numList[i] = Character.getNumericValue(num.charAt(i));
+        }
+
+        for (int i=0;i<numList.length;i++){
+             total = total + numList[i];
+        }
+        System.out.println(total);
+    }
+}
