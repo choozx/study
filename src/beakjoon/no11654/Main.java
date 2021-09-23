a9a7fc00cdb81df3df7d5202a68be381f8340355
jeahyunyoo <choozx23@gmail.com>
2021-09-23 18:49:04 +0900

    printASCII

---
+++ src/beakjoon/no11654/Main.java
@@ -0,0 +1,12 @@
+package beakjoon.no11654;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        char a = sc.nextLine().charAt(0);
+        System.out.println((int)a);
+    }
+}
