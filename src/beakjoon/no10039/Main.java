f3c330e59f770403424b1fdc14f50753fea8385c
jeahyunyoo <choozx23@gmail.com>
2021-10-18 19:41:56 +0900

    averageScore

---
+++ src/beakjoon/no10039/Main.java
@@ -0,0 +1,23 @@
+package beakjoon.no10039;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[5];
+        int sum=0;
+        for (int i = 0;i<5;i++){
+            int k = sc.nextInt();
+            if (k<40){
+                num[i] = 40;
+            } else{
+                num[i] = k;
+            }
+        }
+        for (int i = 0;i<5;i++){
+            sum = sum + num[i];
+        }
+        System.out.println(sum/5);
+    }
+}
